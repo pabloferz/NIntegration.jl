f47b2ef67309ad7886b1fb26fc4292a9c5ce4bd2
Pablo Zubieta <pabloferz@yahoo.com.mx>
2017-03-26 15:12:39 -0600

    First version

--- src/NIntegration.jl
+++ src/NIntegration.jl
@@ -1,5 +1,19 @@
 module NIntegration
 
-# package code goes here
+
+### Dependencies
+using DataStructures
+using StaticArrays
+
+### Implementation
+path = dirname(realpath(@__FILE__)) # works even for symlinks
+include(joinpath(path, "rules.jl"))
+include(joinpath(path, "utils.jl"))
+include(joinpath(path, "types.jl"))
+include(joinpath(path, "integration.jl"))
+
+### Exports
+export nintegrate
+
 
 end # module
